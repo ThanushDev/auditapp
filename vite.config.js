344604e26fd5ed2ca45517832af47619ed1bbde1
Thanush Nethsika <106293021+ThanushDev@users.noreply.github.com>
2026-05-23 13:56:00 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // GitHub Pages වලට අනිවාර්යයි
+  base: './', // <-- මේ පේළිය අනිවාර්යයෙන්ම තියෙන්න ඕනේ!
 })
